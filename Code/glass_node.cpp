a7f5ee3b0ae83dd2ea1d3cab3b4582dd0d1803d4
Marc-AntoineA <marc-antoine.auge@eleves.enpc.fr>
2018-03-27 18:09:06 +0200

    Separation des structures

---
+++ Code/glass_node.cpp
@@ -0,0 +1,23 @@
+#include "glass_node.h"
+
+/////GlassNode/////
+
+GlassNode::GlassNode()
+{
+    plate_id = 0;
+    node_id = 0;
+    pos_x = 0;
+    pos_y = 0;
+    width = 0;
+    height = 0;
+    type = 0;
+    cut = 0;
+    parent = 0;
+    successor_nbr = 0;
+}
+
+GlassNode::~GlassNode()
+{
+    //dtor
+}
+
---
+++ Code/glass_plate.cpp
@@ -0,0 +1,19 @@
+#include "glass_plate.h"
+
+/////GlassPlate/////
+
+GlassPlate::GlassPlate()
+{
+    //ctor
+    waste = 0.0;
+    useful = 0.0;
+    successor_nbr = 0;
+    defect_nbr = 0;
+    residual.Setwidth(0);
+    residual.Setheight(0);
+}
+
+GlassPlate::~GlassPlate()
+{
+    //dtor
+}
